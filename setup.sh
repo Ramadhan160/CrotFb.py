f13fcc12b6b610aea0df0d83ce272cc3de16a737
Putra Maulana <110748674+Ramadhan160@users.noreply.github.com>
2024-06-27 02:03:07 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+clear
+echo
+echo "    ╔══╦══╦═╦╗╔╦╦╦═╦╦══╗╔══╦══╦╗╔══╦══╗" | lolcat
+echo "    ╚╗╔╣╔╗║║║║║║║║║║║╔═╣║╔╗║╔╗║║║╔╗╠║║╝" | lolcat
+echo "    ─║║║╠╣║║║╠╣║║║║║║╚╗║║╔╗║╠╣║╚╣╠╣╠║║╗" | lolcat
+echo "    ─╚╝╚╝╚╩╩═╩═╩═╩╩═╩══╝╚══╩╝╚╩═╩╝╚╩══╝" | lolcat
+echo 
+gem install lolcat
+clear
+echo
+echo "   ╭╮╭━┳━━━┳━━━━┳━━━╮╭╮╭━┳━━━┳━╮╱╭┳╮╭━┳━━━┳━╮╱╭┳━━━╮" | lolcat
+echo "   ┃┃┃╭┫╭━╮┃╭╮╭╮┃╭━╮┃┃┃┃╭┫╭━╮┃┃╰╮┃┃┃┃╭┫╭━╮┃┃╰╮┃┃╭━╮┃" | lolcat
+echo "   ┃╰╯╯┃┃╱┃┣╯┃┃╰┫┃╱┃┃┃╰╯╯┃┃╱┃┃╭╮╰╯┃╰╯╯┃┃╱┃┃╭╮╰╯┃┃╱╰╯" | lolcat
+echo "   ┃╭╮┃┃┃╱┃┃╱┃┃╱┃╰━╯┃┃╭╮┃┃┃╱┃┃┃╰╮┃┃╭╮┃┃╰━╯┃┃╰╮┃┃┃╭━╮" | lolcat
+echo "   ┃┃┃╰┫╰━╯┃╱┃┃╱┃╭━╮┃┃┃┃╰┫╰━╯┃┃╱┃┃┃┃┃╰┫╭━╮┃┃╱┃┃┃╰┻━┃" | lolcat
+echo "   ╰╯╰━┻━━━╯╱╰╯╱╰╯╱╰╯╰╯╰━┻━━━┻╯╱╰━┻╯╰━┻╯╱╰┻╯╱╰━┻━━━╯" | lolcat
+echo 
+clear
+echo
+echo
+echo "       [√] berhasil di install..."
+echo
+echo "     type python CrotFb.py to continue "
+echo
